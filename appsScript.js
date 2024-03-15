f9ccfba72dc3abe248d7e798a35356107bd1b676
AVigants <71322962+AVigants@users.noreply.github.com>
2024-03-15 14:07:46 +0200

    Updated appsScript.js

--- appsScript.js
+++ appsScript.js
@@ -1,37 +1,44 @@
 const properties = [
-  { id: 'GA4-property-1', sheetName: 'sheetName1' },
-  { id: 'GA4-property-2', sheetName: 'sheetName2' },
-    // ... other GA4 properties
+  { id: '123', sheetName: 'ABC' },
+  { id: '456', sheetName: 'DEF' },
+  // more properties
 ];
 
-const SERVICE_ACCOUNT_EMAIL = "something@something.com";
-const PRIVATE_KEY =
-  "-----BEGIN PRIVATE KEY-----    {{PRIVATE KEY}}    -----END PRIVATE KEY-----\n";
+const SERVICE_ACCOUNT_EMAIL = "{{SERVICE_ACCOUNT_EMAIL}}";
+const PRIVATE_KEY = "{{PRIVATE_KEY}}"
+const NOTIF_URL_BASE = `{{NOTIF_URL_BASE}}`;
+const notifyFeatureEnabled = true;
 
 function exportGA4DataToSheets() {
-  Logger.log("Starting exportGA4DataToSheets function.");
   properties.forEach(property => {
-    const data = fetchRealtimeData(property.id);
-    if (!data) {
-      Logger.log(`No data returned for property ${property.id}`);
-      notify(`No data returned for property ${property.id}`);
-      return;
+    try {
+      Logger.log(`[${property.sheetName}] Starting exportGA4DataToSheets function.`);
+      const data = fetchRealtimeData(property.id);
+      if (!data) {
+        Logger.log(`No data returned for property ${property.sheetName}`);
+        notify(`No data returned for property ${property.sheetName}`);
+        return;
+      }
+
+      const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(property.sheetName) ||
+        SpreadsheetApp.getActiveSpreadsheet().insertSheet(property.sheetName);
+      sheet.clear(); // Clear existing data before appending new data
+      sheet.appendRow(["Datetime", "EventName", "EventCount"]); // Header row
+
+      data.forEach(row => {
+        sheet.appendRow([
+          getCurrentDatetime(),
+          row.dimensionValues[0].value,
+          row.metricValues[0].value,
+        ]);
+      });
+
+      Logger.log(`Data successfully written to sheet for property ${property.sheetName}.`);
+
+    } catch (error) {
+      Logger.log(`Couldn't fetch data from property ${property.sheetName}`);
+      notify(`Couldn't fetch data from property ${property.sheetName}`);
     }
-
-    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(property.sheetName) ||
-      SpreadsheetApp.getActiveSpreadsheet().insertSheet(property.sheetName);
-    sheet.clear(); // Clear existing data before appending new data
-    sheet.appendRow(["Datetime", "EventName", "EventCount"]); // Header row
-
-    data.forEach(row => {
-      sheet.appendRow([
-        getCurrentDatetime(),
-        row.dimensionValues[0].value,
-        row.metricValues[0].value,
-      ]);
-    });
-
-    Logger.log(`Data successfully written to sheet for property ${property.id}.`);
   });
 }
 
@@ -68,10 +75,11 @@ function getCurrentDatetime() {
 }
 
 function notify(str) {
-    // Make a GET request to a server:
-  const base = `{{URL BASE}}`;
-  const url = `${base}    ${encodeURIComponent(
-    `[Google Sheets | GA4 Exports]    ${str}`
+  if(!notifyFeatureEnabled){
+    return;
+  }
+  const url = `${NOTIF_URL_BASE}    ${encodeURIComponent(
+    `[Google Sheets - Trodo | GA4 Exports]    ${str}`
   )}`;
   try {
     UrlFetchApp.fetch(url, { muteHttpExceptions: true });
